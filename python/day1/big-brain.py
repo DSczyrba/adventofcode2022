0825989a7670c2ed6a3d1c093fc9d895db7e37ac
DSczyrba <dominic.sczyrba@gmail.com>
2022-12-01 12:46:30 +0000

    added alt version

--- python/day1/big-brain.py
+++ python/day1/big-brain.py
@@ -1,4 +1,18 @@
 with open("input.txt") as f:
-    data = [[y for y in x.split("\n")] for x in f.read().split("\n\n")]
+    data = [[int(y) for y in x.split("\n")] for x in f.read().strip().split("\n\n")]
+    data = sorted([sum(x) for x in data], reverse=True)
 
-print(data)
+print("Lösung 1:", data[0])
+print("Lösung 2:", sum(data[:3]))
+
+exit()
+
+#Alternative:
+data = sorted(
+    (
+        sum(int(y) for y in x.split("\n"))
+        for x
+        in f.read().strip().split("\n\n")
+    ),
+    reverse=True
+)
