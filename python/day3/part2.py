8480f3734ad1ef42d8ce7a19d3df8a7e1730aa17
DSczyrba <dominic.sczyrba@gmail.com>
2022-12-05 19:31:30 +0000

    added day 3

---
+++ python/day3/part2.py
@@ -0,0 +1,16 @@
+with open("input.txt") as f:
+    data = f.read()
+    data = [x for x in data.strip().split("\n")]
+    data = [[x[:len(x)//2], x[len(x)//2:]] for x in data]
+
+score = 0
+for i in range(0, len(data), 3):
+    same_char = list(set(data[i]) & set(data[i+1]) & set(data[i+2]))[0]
+    if same_char.isupper():
+        value = ord(same_char) - 38
+    else:
+        value = ord(same_char) - 96
+    score += value
+
+print(score)
+
